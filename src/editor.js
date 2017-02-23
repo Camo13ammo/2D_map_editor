35099667c717e72246f5f910b71e63769d0786ed
Vincent Leombruno <vleombruno42@gmail.com>
2017-02-23 13:34:45 -0500

    Blue Tile Selected Indicator

--- src/editor.js
+++ src/editor.js
@@ -6,52 +6,57 @@
         this.tileSize = tileSize;
         this.tileCount_X = tileCount_X;
         this.tileCount_Y = tileCount_Y;
-        this.grids = [];
-        this.pallet = new Pallet(tileSize);
-        this.pallet.$canvas.click(event => {
-            const offset = this.pallet.$canvas.offset();
-            const imgData = {
-                img: this.pallet.selectedSheet,
-                x: Math.floor((event.pageX - offset.left) / (this.pallet.tileSize)),
-                y: Math.floor((event.pageY - offset.top) / (this.pallet.tileSize)),
-                dx: this.pallet.tileSize,
-                dy: this.pallet.tileSize
-            };
-            this.grids.forEach(grid => {
-                grid.setSprite(imgData);
-            })
-        });
+        this.grids = [new Grid(tileSize, tileCount_X, tileCount_Y, this)];
+        this.pallet = new Pallet(tileSize, this);
     }
 
     World.prototype.addGrid = function () {
-        let grid = new Grid(this.tileSize, this.tileCount_X, this.tileCount_Y);
-        grid.createAndShow();
-        this.grids.push(grid);
-    }
+        this.grids.push(new Grid(this.tileSize, this.tileCount_X, this.tileCount_Y));
+    };
 
-    function Pallet (tileSize) {
+    function Pallet (tileSize, world) {
+        this.world = world;
         this.selectedSheet = null;
+        this.selectedSpriteData = null;
         this.tileSize = tileSize;
         this.spriteSheets = [];
         this.$canvas = $('<canvas/>');
         this.ctx = this.$canvas[0].getContext("2d");
-        $("#pallet-container").append(this.$canvas);
+        $("#pallet-container").prepend(this.$canvas);
+        const $selectedTileIndicator = $("#selected-tile-indicator");
+        this.$canvas.click(event => {
+            const offset = this.$canvas.offset();
+            $selectedTileIndicator.css({
+                'left': Math.floor((event.pageX - offset.left) / (this.tileSize)) * this.tileSize,
+                'top': Math.floor((event.pageY - offset.top) / (this.tileSize)) * this.tileSize,
+                'min-width': this.tileSize,
+                'height': this.tileSize
+
+            })
+            this.selectedSpriteData = {
+                img: this.selectedSheet,
+                x: Math.floor((event.pageX - offset.left) / (this.tileSize)),
+                y: Math.floor((event.pageY - offset.top) / (this.tileSize)),
+                dx: this.tileSize,
+                dy: this.tileSize
+            };
+        });
     }
 
     // TODO: add functionality for both x and y
     Pallet.prototype.setSpriteSize = function (spriteSizeX, spriteSizeY) {
         this.tileSize = spriteSizeX;
-    }
+    };
 
     Pallet.prototype.addSpriteSheet = function (spriteURL) {
-        let img = new Image();
+        const img = new Image();
         img.onload = function() {
             this.ctx.canvas.height = img.height;
             this.ctx.canvas.width = img.width;
             this.toggleSpriteSheet(this.spriteSheets.push(img)-1);
         }.bind(this);
         img.src = spriteURL;
-    }
+    };
 
     Pallet.prototype.toggleSpriteSheet = function (index) {
         this.selectedSheet = this.spriteSheets[index];
@@ -73,34 +78,17 @@
         this.ctx.lineWidth = 2;
         this.ctx.strokeStyle = "white";
         this.ctx.stroke();
-    }
+    };
 
-    function Grid (tileSize, tileCount_X, tileCount_Y) {
+    function Grid (tileSize, tileCount_X, tileCount_Y, world) {
+        this.world = world;
         this.$canvas = $('<canvas/>');
         this.ctx = this.$canvas[0].getContext("2d");
-        this.spriteData = null;
         this.tileSize = tileSize;
         this.tileCount_X = tileCount_X;
         this.tileCount_Y = tileCount_Y;
         this.dragging = false;
-        this.level = Array(tileCount_Y).fill().map(() => Array(tileCount_X).fill(false));
-    }
-
-    Grid.prototype.draw = function (event) {
-        if (!this.spriteData) return; // Some error for no sprite selected
-        const offset = this.$canvas.offset();
-        const tile_x = Math.floor((event.pageX - offset.left) / (this.tileSize));
-        const tile_y = Math.floor((event.pageY - offset.top) / (this.tileSize));
-        // TODO: Prevent duplicate draws of same image
-        this.ctx.drawImage(this.spriteData.img, // Image source
-            this.spriteData.x * this.spriteData.dx, this.spriteData.y * this.spriteData.dy, //x, y coord start
-            this.spriteData.dx, this.spriteData.dy, // x, y clipping size
-            tile_x * this.tileSize, tile_y * this.tileSize, // x, y placement start
-            this.tileSize, this.tileSize); // x, y placement size
-        this.level[tile_y][tile_x] = true; // TODO: Store image data instead
-    };
-
-    Grid.prototype.createAndShow = function () {
+        this.level = Array(tileCount_Y).fill().map(() => Array(tileCount_X).fill([]));
 
         this.ctx.canvas.width = this.tileCount_X * this.tileSize;
         this.ctx.canvas.height = this.tileCount_Y * this.tileSize;
@@ -139,6 +127,7 @@
             this.draw(event);
         });
 
+        // Update minimap when lifting mouse
         this.$canvas.mouseup(event => {
             this.dragging = false;
             const snap = this.$canvas[0].toDataURL();
@@ -157,49 +146,66 @@
             $preview.show();
             $preview.css({
                 'top': Math.floor((event.pageY - offset.top) / (this.tileSize)) * this.tileSize,
-                'left': Math.floor((event.pageX - offset.left) / (this.tileSize)) * this.tileSize,
+                'left': Math.floor((event.pageX - offset.left) / (this.tileSize)) * this.tileSize
             });
-            if (this.spriteData) {
+            if (this.world.pallet.selectedSpriteData) {
                 $preview.css({
                     'border': 'none',
-                    'background-image': 'url(' + this.spriteData.img.src + ')',
-                    'background-position-x': -1 * (this.spriteData.x * this.spriteData.dx),
-                    'background-position-y': -1 * (this.spriteData.y * this.spriteData.dy),
-                    'opacity': .4,
-                    'width': this.spriteData.dx,
-                    'height': this.spriteData.dy,
+                    'background-size': 100 * (this.world.pallet.ctx.canvas.width / this.world.pallet.tileSize) + '%', // why is this?
+                    'image-rendering': 'pixelated',
+                    'background-image': 'url(' + this.world.pallet.selectedSpriteData.img.src + ')',
+                    'background-position-x': -1 * (this.world.pallet.selectedSpriteData.x * this.tileSize),
+                    'background-position-y': -1 * (this.world.pallet.selectedSpriteData.y * this.tileSize),
+                    'opacity': 0.4,
+                    'min-width': this.tileSize,
+                    'height': this.tileSize,
                 });
             } else {
                 $preview.css({
                     'border': '2px solid black',
                     'min-width': this.tileSize-4,
                     'height': this.tileSize-4,
-                })
+                });
             }
         });
 
         // On Mouse out of grid, hides the sprite preview
         this.$canvas.mouseout(event => {
             $("#sprite-preview").hide();
-        })
+        });
     }
 
-    Grid.prototype.setSprite = function (spriteData) {
-        this.spriteData = spriteData;
-    }
+    Grid.prototype.draw = function (event) {
+        if (!this.world.pallet.selectedSpriteData) return; // Some error for no sprite selected
+        const offset = this.$canvas.offset();
+        const tile_x = Math.floor((event.pageX - offset.left) / (this.tileSize));
+        const tile_y = Math.floor((event.pageY - offset.top) / (this.tileSize));
+        // TODO: Prevent duplicate draws of same image
+        this.ctx.drawImage(
+            this.world.pallet.selectedSpriteData.img, // Image source
+            this.world.pallet.selectedSpriteData.x * this.world.pallet.selectedSpriteData.dx, // x clipping start
+            this.world.pallet.selectedSpriteData.y * this.world.pallet.selectedSpriteData.dy, // y clipping start
+            this.world.pallet.selectedSpriteData.dx, // dx clipping size
+            this.world.pallet.selectedSpriteData.dy, // dy clipping size
+            tile_x * this.tileSize, // x placement start
+            tile_y * this.tileSize, // y placement start
+            this.tileSize, // dx placement size
+            this.tileSize // dy placement size
+        );
+        this.level[tile_y][tile_x] = true; // TODO: Store image data instead
+    };
 
     Grid.prototype.toggle = function () {
         this.$canvas.toggle();
-    }
+    };
 
-    var world = new World(48, 20, 19);
-    world.addGrid();
+    var world = new World(24, 20, 19);
     world.pallet.addSpriteSheet("../sprites/screenshot.png");
-    world.pallet.setSpriteSize(48);
+    world.pallet.setSpriteSize(24);
 
     $("#button").click(function() {
         $("#upload").click();
-    })
+    });
 
     $("#upload").change(function() {
         if (this.files && this.files[0]) {
@@ -211,7 +217,7 @@
 
             reader.readAsDataURL(this.files[0]);
         }
-    })
+    });
 
     // $("#button").click(() => {
 
